75b884852f9d7e95657657e70fa1d61ee5f0282c
Wojtek <mitiolf@outlook.com>
2022-04-20 14:44:41 +0200

    first comi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+import { Download, Features, SectionWrapper } from './components';
+import assets from './assets'
+import styles from './styles/Global';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <SectionWrapper 
+        title="You own store of Nifty NFTs.
+        Start Selling ang Growing."
+        description="Buy, store, collect NFTS,
+        exchange and earn crypto. Join 25 million people
+        using ProNef Marketplace."
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+        />
+        <SectionWrapper 
+        title="Smart User Interface Marketplace"
+        description="Experience a buttery UI of 
+        ProNef NFT Marketplace. Smooth constant 
+        colors of a fluent UI design."
+        mockupImg={assets.homeCards}
+        reverse
+        />
+        <Features />
+        <SectionWrapper 
+        title="Deployment"
+        description="ProNef is built using Expo
+        which runs natively on all users' devices. 
+        You can easily get your app into people's hands."
+        mockupImg={assets.feature}
+        reverse
+        />
+        <SectionWrapper 
+        title="Creative way to showcase the store"
+        description="The app contains two screens. The first screen lists all NFTs while the second one shows the details of a specific NFT."
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
+
+      <Download />
+
+      <div className='px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04'>
+        <p className={`${styles.pText} ${styles.whiteText} pt-6`}>Made with love by {" "} 
+        <b>Wojtek Sordyl</b>
+        </p>  
+      </div>
+    </>
   );
 }
 
